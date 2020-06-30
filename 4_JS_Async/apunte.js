7bfc50730e73ed0b4cc5ea671fea0b4e374417b0
Leon Mesa <leonmesa1979@hotmail.com>
2020-06-29 20:43:26 -0500

    repositorio funcional

---
+++ 4_JS_Async/apunte.js
@@ -0,0 +1,31 @@
+//lenguaje de bajo nivel
+//nos comunicamos con una computadora(Ensamblador)
+
+//lenguaje de alto nivel
+//compilados
+//c, c++ (CODEBLOCKS)
+
+//pseudocompilados
+
+//interpretados
+//javaScript
+//python
+
+
+
+//error
+const pi = 3.1416;
+const array =[];
+const objeto =[];
+
+//funiciones
+const suma = (a, b) => a + b;
+
+const saludo =nombre ("hola") + nombre;
+console.log(suma(1, 5));
+console.log(saludo("gerardo"));
+
+//decodificaciones de carecteres concatenacion
+
+const infoMascota=(nombre, raza, juguete) => "mi mascota se llama" ${nombre}, es una ${raza}, y su juguete favorito es ${juguete};
+console.log(infoMascota("lolo", "salchicha", "pelota"));
---
+++ 5_Request/Funcional.js
@@ -0,0 +1,46 @@
+//........0...1...2...3...4
+const array =[10, 22, 33, 54, 12]
+
+//iterar arreglo => manera imperativa
+//.....0...;0<=5......;1
+for(let i = 0; i<= array.length; i++){
+    console.log(array[i]);
+}
+
+//declarativa
+
+const eacharray = array.forEach((value) => console.log (value));
+console.log(eachArray);
+
+
+//$.....concatenar
+
+var nuevoArreglo = [];
+for(let i = 0; i<= array.length; i++){
+    nuevoArreglo[i] = "${i} - ${array[i]}";
+}
+console.log (nuevoArreglo);
+
+
+//manera declarativa
+const nuevoArreglo = array.map((value)=> "${value} ${index} ${array}");
+console.log(nuevoArreglo);
+
+//imperativas
+const paises = ["mexico", "colombia", "costa Rica", "peru", "Republica Dominicana", "venezuela"];
+
+const nuevoPaises=[];
+let e = 0;
+for(let i = 0; i < paises.length; i++){   
+   if (paises[i].length <= 6){
+       nuevoPaises[e] = paises[i];
+       e++;
+   }
+}
+
+console.log(nuevoPaises);
+
+//declarativas
+const nuevoPaises = paises.filter((value, index)=> value.length <=6);
+
+console.log(nuevoPaises);
